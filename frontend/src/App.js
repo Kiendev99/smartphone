90a71c43f1574115319b36ea85d1e499a30b2f2d
kiendevil99 <trankien2878@gmail.com>
2023-08-05 10:00:25 +0700

    giao dien navbar, home, cart, not-found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 
-import {BrowserRouter, Route, Routes,} from "react-router-dom"
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
 import Navbar  from './components/Navbar';
 import cart from './components/cart';
 import Home from './components/home';
+import Notfound from './components/Notfound';
 function App() {
   return <div className='App'>
     <BrowserRouter>
     <Navbar />
       <Routes>
         <Route path='/cart' Component={cart} />
-        <Route path='/' Component={Home} />
+        <Route path='/not-found'  Component={Notfound} />
+        <Route path='/' exact Component={Home} />
+        <Route path="*" element={<Navigate to="not-found"/>} />
       </Routes>
     </BrowserRouter>
   </div>
---
+++ frontend/src/components/Notfound.jsx
@@ -0,0 +1,8 @@
+const Notfound = () => {
+    return ( <div className="not-found">
+    <h2>404</h2>
+    <p>Page not found</p>
+  </div> );
+}
+ 
+export default Notfound;
